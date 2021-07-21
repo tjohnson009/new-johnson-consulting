e1a0bcc3d96cf6ecf724dddd2d4a3f9da321399a
Timothy Johnson <tjohnson.acts1422@gmail.com>
2021-07-21 13:26:29 -0400

    Styled directory items

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react'; 
 import './directory-item.styles.scss'; 
 
+
 const DirectoryItem = ({ title, imageURL, size }) => {
-    return (<div style={{ backgroundImage: `url(${imageURL})` }} 
-    className={`directory-item ${size}`}>
+    return (<div className={`directory-item ${size}`}>
+        <div className='background-image' style={{ backgroundImage: `url(${imageURL})` }}>
+        
+        </div>
                 <div className='item-content'>
-                    <h1 className='item-title'>{title}</h1>
-                    <span className='item-subtitle'>Explore</span>
+                    <h1 className='item-title'>{title.toUpperCase()}</h1>
+                    <span className='item-subtitle'>EXPLORE</span>
                 </div> 
             </div>
     )
