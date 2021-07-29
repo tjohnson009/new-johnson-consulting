a1edd823a91c8228d919d3d3e848ad4c2d882b4e
Timothy Johnson <tjohnson.acts1422@gmail.com>
2021-07-29 16:57:26 -0400

    Began construction of header component; added SVG for the Johnson COnsulting Logo

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import { Link } from 'react-router-dom';
+import './header.styles.scss'; 
+import {  } from '../../assets/'
+
+const Header = () => {
+    return (
+        <div>
+        <div className='logo'>
+            <Link to='/'>
+            
+            </Link>
+        </div>
+        <div className='link-container'>
+
+        </div>
+        </div>
+    )
+}
+
+export default Header; 
