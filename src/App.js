04809e57530990ec6e661ab0baea636dcfe72e2b
Timothy Johnson <tjohnson.acts1422@gmail.com>
2021-07-29 18:36:41 -0400

    Built and implemented header component; also logo;

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'; 
 import './styles/App.css';
 import HomePage from './pages/homepage/homepage.component'; 
-import ToolsPage from './pages/toolsPage/tools.component'
+import ToolsPage from './pages/toolsPage/tools.component'; 
+import Header from '../src/components/header/header.component'; 
 
 function App() {
   return (<div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/tools' component={ToolsPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,18 +1,20 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom';
 import './header.styles.scss'; 
-import {  } from '../../assets/'
+import { ReactComponent as Logo } from '../../assets/half-white.svg'; 
 
 const Header = () => {
     return (
-        <div>
-        <div className='logo'>
+        <div className='header'>
+        <div className='logo-container'>
             <Link to='/'>
-            
+                <Logo className='logo'/>
             </Link>
         </div>
         <div className='link-container'>
-
+            <Link  to='/tools'className='link'>Tools</Link>
+            <Link to='/sign-in' className='link'>Sign In</Link>
+            <Link  to='/cart' className='link'>Cart</Link>
         </div>
         </div>
     )
