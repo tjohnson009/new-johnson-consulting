90aed2a91a4f4d6d71a9d3c738bbeaebf35c1541
Timothy Johnson <tjohnson.acts1422@gmail.com>
2021-08-10 11:56:19 -0400

    minor modifications to sign up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react'; 
+import FormInput from '../form-input/form-input.component';
+import PersonalizedButton from '../button/personalized-button.component';
 
 class SignUp extends React.Component {
     constructor() {
         super(); 
 
         this.state = ''
+    }
+
+    render() {
+        return (
+            <div>Sign Up Component</div>
+        )
     }
 }
 
